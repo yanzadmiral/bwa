aa3d2a88376064ca88622cecd90d269039248a10
Yayan Cahyani <yayancloud@gmail.com>
2021-04-18 23:35:15 +0700

    3.dsm - membuat post upload image

--- service-media/app.js
+++ service-media/app.js
@@ -1,21 +1,21 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const mediaRouter = require("./routes/media");
 
 const app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(logger("dev"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/media", mediaRouter);
 
 module.exports = app;
--- service-media/routes/media.js
+++ service-media/routes/media.js
@@ -1,9 +1,38 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const isBase64 = require("is-base64");
+const Base64Img = require("base64-img");
+
+const { Media } = require("../models");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post("/", (req, res) => {
+  //res.send('ok brayyy asdasdas');
+  const image = req.body.image;
+
+  if (!isBase64(image, { mimeRequired: true })) {
+    return res.status(400).json({
+      status: "error",
+      message: "invalid base 64",
+    });
+  }
+
+  Base64Img.img(image, "./public/images", Date.now(), async (err, filepath) => {
+    if (err) {
+      return res.status(400).json({ status: "error", message: err.message });
+    }
+    const filename = filepath.split("/").pop();
+    const media = await Media.create({ image: `image/${filename}` });
+
+    return res.json({
+      status: "success",
+      data: {
+        id: media.id,
+        image: `${req.get("host")}/images/${filename}`,
+      },
+    });
+  });
 });
 
 module.exports = router;
