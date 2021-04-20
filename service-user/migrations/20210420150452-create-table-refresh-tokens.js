83007bacd444426f8bca7a3e4a1a6aadce9af3e0
Yayan Cahyani <yayancloud@gmail.com>
2021-04-20 22:27:07 +0700

    4.dsu migration refresh tokens table

--- service-user/migrations/20210420150452-create-table-refresh-tokens.js
+++ service-user/migrations/20210420150452-create-table-refresh-tokens.js
@@ -1,21 +1,44 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable("refresh_tokens", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      token: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+
+    await queryInterface.addConstraint("refresh_tokens", {
+      type: "foreign key",
+      name: "REFRESH_TOKEN_USER_ID",
+      fields: ["user_id"],
+      references: {
+        table: "users",
+        field: "id",
+      },
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    await queryInterface.dropTable("refresh_tokens");
+  },
 };
