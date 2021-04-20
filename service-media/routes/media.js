27f00b66d20ca3938a37481d5f64eb3d2f9fad02
Yayan Cahyani <yayancloud@gmail.com>
2021-04-20 12:55:39 +0700

    3.dsm membuat api delete image

--- service-media/routes/media.js
+++ service-media/routes/media.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const isBase64 = require("is-base64");
 const Base64Img = require("base64-img");
-
+const fs = require('fs');
 const { Media } = require("../models");
 
 /* GET users listing. */
@@ -50,6 +50,37 @@ router.post("/", (req, res) => {
       },
     });
   });
+});
+
+router.delete("/:id", async (req,res)=>{
+  const id = req.params.id;
+
+  const media = await Media.findByPk(id);
+
+  if (!media) {
+    return res.status(400).json({
+      status : "error",
+      message : "media not found"
+    });    
+  }
+
+  fs.unlink(`./public/${media.image}`, async (err)=>{
+    if (err) {
+      return res.status(400).json({
+        status : "error",
+        message : err.message
+      });
+    }
+
+    await media.destroy();
+
+    return res.json({
+      status : "sukses",
+      message : "image deleted"
+    });err.message
+
+  })
+
 });
 
 module.exports = router;
