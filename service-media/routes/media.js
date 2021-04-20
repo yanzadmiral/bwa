ba9dc66d40e3406f46988df69f739da8fd2f3991
Yayan Cahyani <yayancloud@gmail.com>
2021-04-20 12:26:00 +0700

    3.dsm - membuat api get list image

--- service-media/routes/media.js
+++ service-media/routes/media.js
@@ -7,6 +7,23 @@ const Base64Img = require("base64-img");
 const { Media } = require("../models");
 
 /* GET users listing. */
+
+router.get("/", async (req, res) => {
+  const media = await Media.findAll({
+    attributes: ["id", "image"],
+  });
+
+  const mapemedia = media.map((m) => {
+    m.image = `${req.get("host")}/${m.image}`;
+    return m;
+  });
+
+  return res.json({
+    status: "success",
+    data: mapemedia,
+  });
+});
+
 router.post("/", (req, res) => {
   //res.send('ok brayyy asdasdas');
   const image = req.body.image;
@@ -23,7 +40,7 @@ router.post("/", (req, res) => {
       return res.status(400).json({ status: "error", message: err.message });
     }
     const filename = filepath.split("/").pop();
-    const media = await Media.create({ image: `image/${filename}` });
+    const media = await Media.create({ image: `images/${filename}` });
 
     return res.json({
       status: "success",
