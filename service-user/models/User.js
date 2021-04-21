e9a56fb98142e355ee56912e1288cf8c9c647201
Yayan Cahyani <yayancloud@gmail.com>
2021-04-21 13:29:43 +0700

    4.dsu - membuat model user

---
+++ service-user/models/User.js
@@ -0,0 +1,54 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNul: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNul: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNul: false,
+      },
+      role: {
+        type: DataTypes.ENUM,
+        values: ["student", "admin"],
+        allowNul: false,
+        defaultValue: "student",
+      },
+      avatar: {
+        type: DataTypes.STRING,
+        allowNul: true,
+      },
+      profession: {
+        type: DataTypes.STRING,
+        allowNul: true,
+      },
+      createdAt: {
+        field: "created_at",
+        type: DataTypes.DATE,
+        allowNul: false,
+      },
+      updatedAt: {
+        field: "updated_at",
+        type: DataTypes.DATE,
+        allowNul: false,
+      },
+    },
+    {
+      tableName: "Users",
+    }
+  );
+  return User;
+};
