d93ceb7b2a6f438a30b96e447238dff3e2844e27
Yayan Cahyani <yayancloud@gmail.com>
2021-04-21 13:34:14 +0700

    4.dsu - membuat refresh tokens

---
+++ service-user/models/RefreshToken.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, DataTypes) => {
+  const RefreshToken = sequelize.define(
+    "RefreshToken",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      token: {
+        type: DataTypes.TEXT,
+        allowNul: false,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNul: false,
+      },
+      createdAt: {
+        field: "created_at",
+        type: DataTypes.DATE,
+        allowNul: false,
+      },
+      updatedAt: {
+        field: "updated_at",
+        type: DataTypes.DATE,
+        allowNul: false,
+      },
+    },
+    {
+      tableName: "refresh_tokens",
+      tamestaps: true,
+    }
+  );
+  return RefreshToken;
+};
