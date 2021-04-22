491100326eddb0b7fa9af7d5c760df077ae3ffee
Yayan Cahyani <yayancloud@gmail.com>
2021-04-22 22:32:40 +0700

    4.dsu - membuat get user all

--- service-user/routes/users.js
+++ service-user/routes/users.js
@@ -2,13 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Userhandler = require("./handler/users");
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
-
 router.post("/register", Userhandler.register);
 router.post("/login", Userhandler.login);
 router.put("/:id", Userhandler.update);
 router.get("/:id", Userhandler.getuser);
+router.get("/", Userhandler.getusers);
 
 module.exports = router;
