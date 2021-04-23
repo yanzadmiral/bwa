c034ecd4d7f96b69c835e03148b73f4b91d3d116
Yayan Cahyani <yayancloud@gmail.com>
2021-04-23 21:55:20 +0700

    5ijwt - api update

--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const verifyTokenRouter = require("../middleware/verifyToken");
 
 const handlermedia = require('./handler/users');
 /* GET users listing. */
 router.post('/register', handlermedia.register);
 router.post('/login', handlermedia.login);
+router.put('/update',verifyTokenRouter, handlermedia.update);
 
 module.exports = router;
