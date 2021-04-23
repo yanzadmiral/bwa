94ca2626b51854b76162659e0a0e36675c09d43f
Yayan Cahyani <yayancloud@gmail.com>
2021-04-23 22:03:38 +0700

    5.ijwt api get profile

--- api-gateway/routes/handler/users/index.js
+++ api-gateway/routes/handler/users/index.js
@@ -1,9 +1,11 @@
 const register = require('./register');
 const login = require('./login');
 const update = require('./update');
+const getuser = require('./getUser');
 
 module.exports = {
     register,
     login,
-    update
+    update,
+    getuser
 };
--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -7,5 +7,6 @@ const handlermedia = require('./handler/users');
 router.post('/register', handlermedia.register);
 router.post('/login', handlermedia.login);
 router.put('/update',verifyTokenRouter, handlermedia.update);
+router.get('/',verifyTokenRouter, handlermedia.getuser);
 
 module.exports = router;
