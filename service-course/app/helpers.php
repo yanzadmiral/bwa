a318cc31bd7148660cf4549f3440ab3f3ea1e507
Yayan Cahyani <yayancloud@gmail.com>
2021-04-25 22:26:20 +0700

    6.dsc - membuat function helper

--- service-course/app/helpers.php
+++ service-course/app/helpers.php
@@ -1,5 +1,45 @@
 <?php
-function test()
+use Illuminate\Support\Facades\Http;
+
+function getUser($userId)
 {
-    return "just testing";
+    $url = env('SERVICE_USER_URL')."users/".$userId;
+
+    try {
+        $response = Http::timeout(10)->get($url);
+        $data = $response->json();
+        $data['http_code'] = $response->getStatusCode();
+        return $data;
+    } catch (\Throwable $th) {
+        return [
+            'status' => 'error',
+            'http_code' => 500,
+            'message' => 'service user unavailable'
+        ];
+    }
+}
+
+function getUserByIds($userIds = [])
+{
+    $url = env('SERVICE_USER_URL')."users/".$userIds;
+
+    try {
+        if (count($userId) === 0) {
+            return [
+                "status" => 'success',
+                'http_code' => 200,
+                'data' => []
+            ];
+        }
+        $response = Http::timeout(10)->get($url, ['user_ids[]' => $userIds]);
+        $data = $response->json();
+        $data['http_code'] = $response->getStatusCode();
+        return $data;
+    } catch (\Throwable $th) {
+        return [
+            'status' => 'error',
+            'http_code' => 500,
+            'message' => 'service user unavailable'
+        ];
+    }
 }
