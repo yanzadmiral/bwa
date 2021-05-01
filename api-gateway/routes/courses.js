689cf7b8d0f5899d2b66055f84800c03b9222234
Yayan Cahyani <yayancloud@gmail.com>
2021-05-01 21:11:39 +0700

    7.isc api course part 3

--- api-gateway/routes/courses.js
+++ api-gateway/routes/courses.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-
+const verifyToken = require("../middleware/verifyToken");
 const handlermedia = require('./handler/courses');
 /* GET users listing. */
 router.get('/', handlermedia.getall);
-// router.get('/:id', handlermedia.get);
-router.post('/', handlermedia.create);
-router.put('/:id', handlermedia.update);
-router.delete('/:id', handlermedia.destroy);
+router.get('/:id', handlermedia.get);
+
+router.post('/', verifyToken,handlermedia.create);
+router.put('/:id', verifyToken,handlermedia.update);
+router.delete('/:id', verifyToken,handlermedia.destroy);
 
 module.exports = router;
